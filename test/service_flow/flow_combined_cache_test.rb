266cb85f693efd506809b7de800948dae3c9c3e0
Justin Jia <justinj656@live.cn>
2016-01-14 16:04:42 +0800

    update flow_combined_cache_test

--- test/service_flow/flow_combined_cache_test.rb
+++ test/service_flow/flow_combined_cache_test.rb
@@ -4,14 +4,14 @@ require_relative 'flow_cases'
 require 'web_api'
 require 'service_flow'
 
-class FlowCacheSplitTest < Minitest::Test
+class FlowCombinedCacheTest < Minitest::Test
   include FlowCases
 
   def setup
     @flow = ::ServiceFlow::Flow.new RawFlows['dining']
   end
 
-  def test_run_combined_cache
+  def ntest_run_combined_cache
     @flow.transform! :combined
 
     # puts @flow.actions.inspect
@@ -25,6 +25,35 @@ class FlowCacheSplitTest < Minitest::Test
 
     puts 'cache log ==', @flow.cache_log(:statistic).inspect
 
+  end
+
+  def test_cache_pool
+    size_in_mb = 4
+    ::Cache::CachePool.capacity = 1024 * 1024 * size_in_mb
+
+    flow = ServiceFlow::Flow.new RawFlows['dining']
+    flow.transform! :combined
+    # puts flow.split_scheme.inspect
+
+    times = 0
+    begin
+      10000.times do
+        flow.start
+        puts times+=1
+        sleep 0.15
+      end
+    rescue
+      puts "error:#{$!} at:#{$@}"
+    ensure
+      write_res "combined_cache_pool_log_#{size_in_mb}", ::Cache::CachePool.log.to_yaml
+      write_res "combined_exec_log_#{size_in_mb}", {stat: flow.log(:s), raw: flow.log}.to_yaml
+      write_res "combined_cache_log_#{size_in_mb}", {stat: flow.cache_log(:s), raw: flow.cache_log}.to_yaml
+    end
+
+  end
+
+  def write_res(file_name, data)
+    File.open(::PathConstant::LogRoot.join("#{file_name}_#{Time.now.strftime('%Y%m%d_%H%M%S')}.yml"), 'w').write(data) 
   end
 
 end
